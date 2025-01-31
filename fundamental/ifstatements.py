edd51836070b0a397593ba8509ecb8eec44cd29a
mohanr01 <mohanthink91@gmail.com>
2025-01-31 08:33:53 +0530

    fundamentals

---
+++ fundamental/ifstatements.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 31 07:50:44 2025
+
+@author: Mohan
+"""
+
+print(4*10)
+a=10
+if a>10:
+    print("value is greater than",a)
+elif a>=10:
+    print("value is greater than equal",a)
+else:
+    print("value is lesser than",a)
+    
+    
+'''
+multipe line statement break
+use back slash for continuation of line
+'''
+sentence = "This is a very long sentence that we want to " \
+           "split over multiple lines for better readability."
+
+print(sentence)
+
+uservalue = input("enter the console value: ")
+print(uservalue,type(uservalue))
+        
